3fbdde69ccb0471dba1f373e0f7c3d863db91fd3
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-07-30 16:48:36 -0300

    Router and Pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
