abcba94d48e51c5dcf012b08bb1f036f1231146b
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-08-01 01:24:22 -0300

    App.css implementation

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/index.jsx";
 import Shop from "./pages/shop/index.jsx";
 import About from "./pages/about/index.jsx";
+import NavBar from "./components/navBar/index.jsx";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
